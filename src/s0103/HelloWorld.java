434e3297c8ff36087616d61db3fcceffd97710dc
pahkey <pahkey@gmail.com>
2023-06-30 11:25:14 +0900

    initial commit

---
+++ src/s0103/HelloWorld.java
@@ -0,0 +1,7 @@
+package s0103;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/s0301/Sample0301_2.java
@@ -0,0 +1,13 @@
+package s0301;
+
+class Sample0301_2 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+    }
+}
+
